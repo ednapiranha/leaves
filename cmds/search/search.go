7864cc6090b8d8f7b63f5ae2567b892dc4eb8656
Jennifer Fong <error@error.media>
2016-07-07 22:32:15 -0700

    search script fix

--- cmds/search/search.go
+++ cmds/search/search.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	//"fmt"
-	//"log"
-	//"regexp"
-	//"strings"
+	"fmt"
+	"log"
+	"regexp"
+	"strings"
 
 	"github.com/revolting/leaves/db"
 )
@@ -13,23 +13,15 @@ var d = db.NewDB("./boltdb/leaves.db")
 
 func main() {
 	// cleans up strain names for searching
-	/*
-		strains, _ := db.GetAllStrainsNoPagination(d)
+	strains, _ := db.GetAllStrainsNoPagination(d)
 
-		reg, err := regexp.Compile("[()-/]")
-		if err != nil {
-			log.Fatal(err)
-		}
+	reg, err := regexp.Compile("[()-/]")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-		for i := range strains {
-			strains[i].SearchTerm = reg.ReplaceAllString(strings.ToLower(strains[i].Name), "")
-			db.UpdateStrain(strains[i], d)
-		}
-	*/
-	// fix order id for older posts
-	reviews, _ := db.GetFeedNoPagination(d)
-	for i := range reviews {
-		reviews[i].OrderId = db.GetMaxOrder() - reviews[i].CreatedAt
-		db.UpdateReview(reviews[i], d)
+	for i := range strains {
+		strains[i].SearchTerm = reg.ReplaceAllString(strings.ToLower(strains[i].Name), "")
+		db.UpdateStrain(strains[i], d)
 	}
 }
